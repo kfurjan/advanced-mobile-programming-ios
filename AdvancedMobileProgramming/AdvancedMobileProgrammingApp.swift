0f826a786af4636d5862063c7ce58bd7ea652d34
Kevin Furjan <kevin.furjan@ericsson.com>
2023-07-24 16:23:27 +0200

    Initial Commit

---
+++ AdvancedMobileProgramming/AdvancedMobileProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedMobileProgrammingApp.swift
+//  AdvancedMobileProgramming
+//
+//  Created by Ericsson on 24.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct AdvancedMobileProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
